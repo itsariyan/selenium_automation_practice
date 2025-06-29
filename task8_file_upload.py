ae1465bff3ad7c868f1cffb2b8dce984c1b6029a
itsariyan <masrurk978@gmail.com>
2025-06-29 10:00:36 -0700

    Update task8_file_upload.py

--- task8_file_upload.py
+++ task8_file_upload.py
@@ -3,9 +3,9 @@ from selenium.webdriver.common.by import By
 import time
 driver=webdriver.Chrome()
 driver.get("https://the-internet.herokuapp.com/upload")
-upload=driver.find_element(By.ID,"file-upload")
-upload.send_keys("C:\Users\Personal\Pictures\download.jpeg")
-uploadkey=driver.find_element(By.ID,"file-submit")
+upload = driver.find_element(By.ID, "file-upload")
+upload.send_keys(r"C:\Users\Personal\Pictures\download.jpeg")  
+uploadkey = driver.find_element(By.ID, "file-submit")
 uploadkey.click()
 
 driver.quit()
