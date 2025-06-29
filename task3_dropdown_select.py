19d1fa21644c51b49a2099f33653becda301e86f
itsariyan <masrurk978@gmail.com>
2025-06-29 08:59:27 -0700

    Create task3_dropdown_select.py

---
+++ task3_dropdown_select.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+driver=webdriver.Chrome()
+driver.get("https://the-internet.herokuapp.com/dropdown")
+element=driver.find_element(By.ID,"dropdown")
+options=Select(element)
+
+options.select_by_value("1")
+print("Option 1 has been selected")
